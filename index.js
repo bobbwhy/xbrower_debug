a1f2f26059f27a8a36627676f23babe8f77bc966
Robert Lamarca <robertlamarca@gmail.com>
2015-01-31 14:22:02 -0500

    fixed missing index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-src/index.js
+
+
+
+/* 
+
+
+
+*/
+
+
+
+
+module.exports = { 
+	attach:require("./src/___attach"),
+	Atom:require("./src/xmit_atom"),
+	ChannelAtom:require("./src/channel_atom"),
+	ChannelXform:require("./src/channel_xform"),
+	XmitThen:require("./src/xmit_then")
+}
+
+
